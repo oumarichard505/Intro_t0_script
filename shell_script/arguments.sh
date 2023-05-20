55c98b76e7886ab28810daa883b39d9fde734c03
Richard Ouma <richard.ouma@student.moringaschool.com>
2023-05-20 11:32:47 +0300

    arguments

---
+++ shell_script/arguments.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+echo $0 #name of the file
+echo $1 
+echo $2
---
+++ shell_script/user_input.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+read -p "Enter your name: " NAME
+read -sp "Enter your password" PASSWORD
+
+echo "Your name is $NAME and password is $PASSWORD"
