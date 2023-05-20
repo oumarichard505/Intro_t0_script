da0a2e478613bd90e22f4db788446497e63cff53
Richard Ouma <richard.ouma@student.moringaschool.com>
2023-05-20 11:15:44 +0300

    Adding the variables

---
+++ shell_script/variable.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#sytem variables
+
+echo "user: $USER"
+echo "Working directory $PWD"
+
+#user_defined variables
+
+AGE=90
+NAME="Wayne Rooney"
+
+
+echo "Your name is $NAME, you are $AGE old"
+
